8fc1a5b5e2a30d8ab1b106214007545297bd441b
timur4923 <139814300+timurko7674@users.noreply.github.com>
2025-02-02 18:44:21 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,27 @@
+const https = require('https');
+const { URL } = require('url');
+
 module.exports = (req, res) => {
-  const url = req.query.url;
+  const { url } = req.query;
+
   if (!url) {
-    return res.status(400).json({ error: "No URL provided" });
+    return res.status(400).json({ error: 'URL parameter is required' });
   }
 
-  fetch(url)
-    .then((response) => response.text())
-    .then((data) => res.send(data))
-    .catch((error) => res.status(500).json({ error: error.message }));
+  try {
+    const targetUrl = new URL(url);
+    if (targetUrl.protocol !== 'https:') {
+      return res.status(400).json({ error: 'Only HTTPS URLs are supported' });
+    }
+
+    https.get(targetUrl, (response) => {
+      res.status(200).setHeader('Content-Type', 'text/html');
+      response.pipe(res); // Pipe the response from the target URL to the client
+    }).on('error', (e) => {
+      res.status(500).json({ error: 'Error while fetching URL' });
+    });
+
+  } catch (error) {
+    return res.status(400).json({ error: 'Invalid URL' });
+  }
 };
